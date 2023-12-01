4a8844a1903fe1c613dd62764afb3c7d71e44235
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-12-01 16:04:05 +0200

    Add FileAttributes class for utilization with the other APIs
    This can be used by external API types to map files to objects
    through mutations.

--- lib/decidim/apifiles/api.rb
+++ lib/decidim/apifiles/api.rb
@@ -4,5 +4,6 @@ module Decidim
   module Apifiles
     autoload :BlobType, "decidim/api/blob_type"
     autoload :BlobMutationType, "decidim/api/blob_mutation_type"
+    autoload :FileAttributes, "decidim/api/file_attributes"
   end
 end
