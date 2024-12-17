497631368687df67294eff62d376a881cbc14851
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2024-12-17 10:58:52 +0200

    Find blob by either the signed_id or id

--- lib/decidim/apifiles/query_extensions.rb
+++ lib/decidim/apifiles/query_extensions.rb
@@ -12,12 +12,20 @@ module Decidim
       # Returns nothing.
       def self.included(type)
         type.field :blob, BlobType, null: true, description: "Finds a blob" do
-          argument :id, GraphQL::Types::ID, description: "The ID of the blob", required: true
+          argument :id, GraphQL::Types::ID, required: false, description: "The ID of the blob"
+          argument :signed_id, GraphQL::Types::String, required: false, description: "The signed ID of the blob"
         end
       end
 
-      def blob(id:)
-        ActiveStorage::Blob.find_by(id: id)
+      # Retrieves a blob by ID or signed ID
+      def blob(id: nil, signed_id: nil)
+        if id
+          ActiveStorage::Blob.find_by(id: id)
+        elsif signed_id
+          ActiveStorage::Blob.find_signed(signed_id)
+        else
+          raise GraphQL::ExecutionError, "Either 'id' or 'signedId' must be provided."
+        end
       end
     end
   end
